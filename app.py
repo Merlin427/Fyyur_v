bda0061ffd0938e4e72a58e5a28a7185c0288bb1
danievanrensburg@hotmail.com <danievanrensburg@hotmail.com>
2020-07-23 09:17:38 +0200

    fixed migration issue after altering models

--- app.py
+++ app.py
@@ -31,21 +31,7 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-class Genre(db.Model):
-    __tablename__ = 'genre'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-
-venue_genre_table = db.Table('venue_genre_table',
-    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'), primary_key=True),
-    db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'), primary_key=True)
-)
-
-artist_genre_table = db.Table('artist_genre_table',
-    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'), primary_key=True),
-    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True)
-)
 
 class Venue(db.Model):
     __tablename__ = 'venue'
@@ -58,7 +44,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref ('venues', lazy=True))
+    genres = db.Column(db.ARRAY(db.String))
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(120))
@@ -81,7 +67,7 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref ('artists'))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
